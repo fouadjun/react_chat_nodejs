1ef405d47b943b1dba37c1709d2ca5b15a8d9850
Fuad <fouad.jun@gmail.com>
2020-05-18 17:26:46 +0400

    fix socket

--- socket.js
+++ socket.js
@@ -1,21 +1,33 @@
 const socket_io = require('socket.io');
+const Auth = require('./services/auth');
 
 const socket = (server) => {
     const io = socket_io(server);
+    let sockets = [];
 
     io.use(function (socket, next) {
-        console.log('socket is connected', socket.handshake.query.auth_token);
+        console.log('socket is connected');
 
-
-        next();
+        Auth.checkAuth(socket.handshake.query.auth_token).then(r => {
+            sockets[socket.id] = socket;
+            next();
+        }).catch(() => {
+            socket.disconnect();
+        });
     });
 
     io.on('connection', function(socket) {
         console.log('new connection!', socket.id);
 
-        socket.emit("helloBro", {data: { name: 'Fuad' }});
+        //socket.emit("helloBro", {data: { name: 'Fuad' }});
+
+        socket.on('send', function (socket) {
+            console.log(socket);
+        })
 
     });
+
+
 
 
 };
